2793a786c8310fa9e4cd33fe0ea411ec18090ee1
Gian Antonio Batista <gian.antoniobatista@gmail.com>
2020-01-12 15:44:36 -0300

    Implementando o método findById()

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,23 +1,20 @@
 package aplicacao;
 
 import java.text.ParseException;
-import java.util.Date;
 
 import modelo.dao.DaoFabrica;
 import modelo.dao.VendedorDao;
-import modelo.entidades.Departamento;
 import modelo.entidades.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) throws ParseException {
-		Departamento dpt = new Departamento(1, "Livros");
-		Vendedor vd = new Vendedor(1, "Gian", "gian@gmail.com", new Date(), 200.0, dpt);
+		
 		VendedorDao vdao = DaoFabrica.criarVendedorDao();
 		
-		System.out.println(dpt);
+		Vendedor v = vdao.findById(3); 
 		
-		System.out.println(vd);
+		System.out.println(v);
 
 	}
 
--- src/modelo/dao/impl/VendedorDaoJDBC.java
+++ src/modelo/dao/impl/VendedorDaoJDBC.java
@@ -1,34 +1,84 @@
 package modelo.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import modelo.dao.VendedorDao;
+import modelo.entidades.Departamento;
 import modelo.entidades.Vendedor;
 
-public class VendedorDaoJDBC implements VendedorDao{
+public class VendedorDaoJDBC implements VendedorDao {
+
+	private Connection conn;
+
+	public VendedorDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Vendedor obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Vendedor obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Vendedor findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.DepName\r\n" + 
+					"FROM seller INNER JOIN department\r\n" + 
+					"ON seller.DepartmentId = department.Id\r\n" + 
+					"WHERE department.Id = ?");
+
+			st.setInt(1, id);
+			rs = st.executeQuery();
+
+			if (rs.next()) {
+				Departamento dep = new Departamento();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setNome(rs.getString("DepName"));
+				
+				Vendedor obj = new Vendedor();
+				obj.setId(rs.getInt("Id"));
+				obj.setNome(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setSalario(rs.getDouble("BaseSalary"));
+				obj.setDataAniversario(rs.getDate("BirthDate"));
+				obj.setDepartamento(dep);
+				
+				return obj;
+			}
+			return null;
+
+		} catch (SQLException e) {
+			
+			throw new DbException(e.getMessage());
+			
+		}finally {
+			
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);			
+		}
+		
 	}
 
 	@Override
