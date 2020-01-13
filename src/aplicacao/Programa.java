397223b489884ae07a60672bc728413964033b83
Gian Antonio Batista <gian.antoniobatista@gmail.com>
2020-01-13 13:04:43 -0300

    Implementando o método Insert()

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,6 +1,7 @@
 package aplicacao;
 
 import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
 import modelo.dao.DaoFabrica;
@@ -14,19 +15,22 @@ public class Programa {
 		
 		VendedorDao vdao = DaoFabrica.criarVendedorDao();
 		
-		System.out.println("teste 1: vendedor findById");
+		System.out.println("\n=== teste 1: vendedor findById");
 		Vendedor v = vdao.findById(3); 
 		System.out.println(v);
 		
-		System.out.println("teste 2: vendedor findByDepartment");
+		System.out.println("\n=== teste 2: vendedor findByDepartment");
 		List<Vendedor> dpto = vdao.findByDepartment(new Departamento(2, null));
 		dpto.forEach(System.out::println);
 		
-		System.out.println("teste 3: vendedor findAll");
+		System.out.println("\n=== teste 3: vendedor findAll");
 		dpto = vdao.findAll();
 		dpto.forEach(System.out::println);
 		
-		
+		System.out.println("\n=== teste 4: vendedor inset");
+		Vendedor vendedor = new Vendedor(null, "Giliard", "giliard@gmail.com", new Date(), 5000.0, new Departamento(2, null));
+		vdao.insert(vendedor);
+		System.out.println("Insert: " + vendedor.getId());
 	}
 
 }
